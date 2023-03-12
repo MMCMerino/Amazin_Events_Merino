d933ff20fe67e146f5d5d0173fdb33a42a14a6d7
Maria Marta Merino <mariamartamerino@gmail.com>
2023-03-12 12:12:22 -0300

    logica cambiada a funciones de orden sup

--- scripts/past_e.js
+++ scripts/past_e.js
@@ -2,27 +2,31 @@
 
 const divTarjetas = document.getElementById("tarjetas");
 
-let eventos ='';
+
+const pastEvents = data.events.filter( evento => evento.date < data.currentDate);
+console.log (pastEvents);
+
+const eventos = pastEvents.reduce((acumulador, valorActual) => {
+  return acumulador + `<div class="container2 ">
+                       <div class="card " style="width: 18rem;">
+                        <img src= ${valorActual.image} class="card-img-top " alt="Imagen Evento: ${valorActual.name}">
+                           <div class="card-body">
+                            <h5 class="card-title">${valorActual.name}</h5>
+                             <p class="card-text">${valorActual.description}</p>
+                            <p class="card-text">Fecha: ${valorActual.date}</p>
+                              <p class="card-text ">Price: ${valorActual.price}</p>
+                              <a href="./details.html" class="btn btn-danger">Details</a>
+                           </div>
+
+                       </div>
+                      </div>`
+                },"");
 
 
-for (let i = 0; i < data.events.length; i++){
-
-   if(data.events[i].date < data.currentDate){
-        eventos += ` <div class="container2 ">
-                    <div class="card " style="width: 18rem;">
-                      <img src= ${data.events[i].image} class="card-img-top " alt="Imagen Evento">
-                      <div class="card-body">
-                      <h5 class="card-title">${data.events[i].name}</h5>
-                      <p class="card-text">${data.events[i].description}</p>
-                      <p class="card-text">Fecha: ${data.events[i].date}</p>
-                      <p class="card-text ">Price: ${data.events[i].price}</p>
-                      <a href="./details.html" class="btn btn-danger">Details</a>
-                      </div>
-  
-                    </div>
-                 </div>`
-                }
-            }
 
 console.log(eventos);
-divTarjetas.innerHTML = eventos;
+divTarjetas.innerHTML = eventos;             
+
+
+
+
